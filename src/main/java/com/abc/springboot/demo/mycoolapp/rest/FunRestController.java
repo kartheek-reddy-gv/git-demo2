20a4821fc4cd0aa1c0cae5b2ddfac1aef944cfa5
kartheek-reddy-gv <gv.kartheekreddy@gmail.com>
2021-01-31 20:59:06 +0530

    first commit of git demo

---
+++ src/main/java/com/abc/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,28 @@
+package com.abc.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@Value("${coach.name")
+	private String coachName;
+	
+	@Value("${team.name")
+	private String teamName;
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + "Team name: "+ teamName;
+		
+	}
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world! Time on server is " + LocalDateTime.now();
+	}
+}
