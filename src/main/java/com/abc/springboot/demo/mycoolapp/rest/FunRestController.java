357be0b6e2163f719a8443d9eec7a6c4ebd0ff48
kartheek-reddy-gv <gv.kartheekreddy@gmail.com>
2021-02-05 18:33:19 +0530

    changed to simple java programm

--- src/main/java/com/abc/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/abc/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -6,22 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class FunRestController {
 	
-	@Value("${coach.name")
-	private String coachName;
-	
-	@Value("${team.name")
-	private String teamName;
-	
-	@GetMapping("/teaminfo")
-	public String getTeamInfo() {
-		return "Coach: " + coachName + "Team name: "+ teamName;
-		
-	}
-	
-	@GetMapping("/")
 	public String sayHello() {
 		return "Hello Kartheek! Time on server is " + LocalDateTime.now();
 	}
